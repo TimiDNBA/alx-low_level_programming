7f189ec8f3154168e266fc11c5c77a54f0cd6b3f
Oluwatimilehin Ojediran <dnba.creation@gmail.com>
2022-09-11 14:59:23 -0700

    print C program to a standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *s= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	while (*s) putchar(*s++);
+	return (1)
+}
