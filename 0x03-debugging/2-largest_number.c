1b6ee3e98c6fd7b50d6e9dd84444855e9d2edf77
Oluwatimilehin Ojediran <dnba.creation@gmail.com>
2022-09-16 18:16:31 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,18 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
-	if (a > b && b > c)
-	{
+int largest;
+
+if (a > b && b > c)
+{
 	largest = a;
-	}
-	else if (b > a && a > c)
-	{
+}
+else if (b > a && a > c)
+{
 	largest = b;
-	}
-	else
-	{
+}
+else if (c < b)
+{
 	largest = c;
-	}
-	return (largest);
+}
+else
+{
+	largest = b;
+}
+
+return (largest);
 }
