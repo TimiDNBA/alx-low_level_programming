8087cead32825aec2f49b0ae6791d978f4935400
Oluwatimilehin Ojediran <dnba.creation@gmail.com>
2022-09-20 10:22:05 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * reset_to_98 - takes a pointer and updates the value
+ * @n: pointer to be updated
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
